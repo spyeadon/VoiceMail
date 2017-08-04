0d4576e856a519ce88773bccdc66cd8fe5302f02
Soren Yeadon <spyeadon@gmail.com>
2017-08-03 19:00:27 -0500

    found way to import bootstrap-sass files

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -11,28 +11,30 @@ const Navbar = connect(
 )(
   ({user}) =>
     <nav className="navbar">
-      <div id="navbar-left" >
-        <img src="mail-icon.ico" id="logo" />
-        {user ?
-          <div id="menu-btn-container">
-            <Link to="/mailbox">
-              <button className="btn btn-default btn-lg menu-buttons">
-                Mailbox
-              </button>
-            </Link>
-            <Link to="/account">
-              <button className="btn btn-default btn-lg menu-buttons">
-                Account Settings
-              </button>
-            </Link>
-          </div> :
-        null}
-        {user ? <Paging /> : null}
-      </div>
-      <div id="navbar-right">
-        {user ? <FilterContainer /> : null}
-        {user ? <WhoAmI /> : null}
-        {user ? <img src={user.img_url} id="profile-photo" /> : null}
+      <div className="container-fluid">
+        <div id="navbar-left" >
+          <img src="mail-icon.ico" id="logo" />
+          {user ?
+            <div id="menu-btn-container">
+              <Link to="/mailbox">
+                <button className="btn btn-default btn-lg menu-buttons">
+                  Mailbox
+                </button>
+              </Link>
+              <Link to="/account">
+                <button className="btn btn-default btn-lg menu-buttons">
+                  Account Settings
+                </button>
+              </Link>
+            </div> :
+          null}
+          {user ? <Paging /> : null}
+        </div>
+        <div id="navbar-right">
+          {user ? <FilterContainer /> : null}
+          {user ? <WhoAmI /> : null}
+          {user ? <img src={user.img_url} id="profile-photo" /> : null}
+        </div>
       </div>
     </nav>
   )
