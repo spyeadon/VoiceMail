f6c5d6809871d4cbd38920b0b28733d0b0986692
Soren Yeadon <spyeadon@gmail.com>
2017-09-01 17:39:27 -0500

    audio recording container and dropdown now working with jquery and bootstrap

--- app/components/AudioContainer.jsx
+++ app/components/AudioContainer.jsx
@@ -7,14 +7,20 @@ const AudioContainer = connect(null, null)(
       <div id="audio-container">
         <button
           id="audio-dropdown"
+          type="button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
           >Record VoiceMail <span className="caret" />
         </button>
-        <ul id="audio-menu" >
-          <li><a href="#">Start Recording</a></li>
-          <li>Stop Recording</li>
-          <li>Play Recording</li>
-          <li>Clear Recording</li>
-          <li>Attach To Email</li>
+        <ul id="audio-menu" aria-labelledby="audio-dropdown">
+          <li><a className="audio-menu-btn">Start Recording</a></li>
+          <li><a className="audio-menu-btn">Stop Recording</a></li>
+          <li className="divider" />
+          <li><a className="audio-menu-btn">Play Recording</a></li>
+          <li><a className="audio-menu-btn">Clear Recording</a></li>
+          <li className="divider" />
+          <li><a className="audio-menu-btn">Attach To Email</a></li>
         </ul>
       </div>
     </li>
