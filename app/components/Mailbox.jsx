f16bbfb0b2575a52aac0ae8ac529b0755cba2190
Soren Yeadon <spyeadon@gmail.com>
2017-09-05 01:17:29 -0500

    implementing mail threads component with bootstrap table

--- app/components/Mailbox.jsx
+++ app/components/Mailbox.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Sidebar from './Sidebar.jsx'
 import MailContainer from './Mail.jsx'
+import TableMailContaier from './Table-Mail.jsx'
 
 const Mailbox = props => {
   return (
     <div id="mailbox-container" >
       <Sidebar />
-      <MailContainer />
+      <TableMailContaier />
     </div>
   )
 }
