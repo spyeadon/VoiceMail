d122d8313bae4a6406221bf66c6f2d602aeff8ab
Soren Yeadon <spyeadon@gmail.com>
2017-11-13 18:11:54 -0600

    working on thread container sidebar issue

--- app/components/Mail.jsx
+++ app/components/Mail.jsx
@@ -20,45 +20,45 @@ const Mail = props => {
     <div id="mail-container">
       {
         threads.map(thread =>
-        <div className="threads-container" key={thread.threadId} >
-          <div
-            className="threads-LI"
-            onClick={() => {
-              if (props.currentThreadId === thread.threadId){
-                props.setCurrentThread()
-                props.setCurrentMessage()
+          <div className="threads-container" key={thread.threadId} >
+            <div
+              className="threads-LI"
+              onClick={() => {
+                if (props.currentThreadId === thread.threadId){
+                  props.setCurrentThread()
+                  props.setCurrentMessage()
+                }
+                else {
+                  props.setCurrentThread(thread.threadId)
+                  props.setCurrentMessage()
+                }
+              }}>
+              {props.currentThreadId === thread.threadId ?
+                <span className="threads-from-address-HL">
+                  {thread.messages[0].headers.From.split('<')[0] || thread.messages[0].headers['Return-Path']}
+                </span> :
+                <span className="threads-from-address">
+                  {thread.messages[0].headers.From.split('<')[0] || thread.messages[0].headers['Return-Path']}
+                </span>
               }
-              else {
-                props.setCurrentThread(thread.threadId)
-                props.setCurrentMessage()
-              }
-            }}>
-            {props.currentThreadId === thread.threadId ?
-              <span className="threads-from-address-HL">
-                {thread.messages[0].headers.From.split('<')[0] || thread.messages[0].headers['Return-Path']}
-              </span> :
-              <span className="threads-from-address">
-                {thread.messages[0].headers.From.split('<')[0] || thread.messages[0].headers['Return-Path']}
+              <span className="threads-subject-line">
+                {thread.messages[0].headers.Subject} &mdash;
               </span>
+              <span className="threads-snippet">
+                {thread.snippet}
+              </span>
+            </div>
+            {props.currentThreadId === thread.threadId ?
+              <Thread
+                currentThread={thread}
+                currentThreadId={props.currentThreadId}
+                setCurrentThread={props.setCurrentThread}
+                setCurrentMessage={props.setCurrentMessage}
+                currentMessageId={props.currentMessageId}
+              />
+              : null
             }
-            <span className="threads-subject-line">
-              {thread.messages[0].headers.Subject} &mdash;
-            </span>
-            <span className="threads-snippet">
-              {thread.snippet}
-            </span>
           </div>
-          {props.currentThreadId === thread.threadId ?
-            <Thread
-              currentThread={thread}
-              currentThreadId={props.currentThreadId}
-              setCurrentThread={props.setCurrentThread}
-              setCurrentMessage={props.setCurrentMessage}
-              currentMessageId={props.currentMessageId}
-            />
-            : null
-          }
-        </div>
         )
       }
     </div>
